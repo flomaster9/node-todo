cc7d24c213580d8d5c1dd0114a17468ec474f889
flomaster <flomaster9@mail.ru>
2017-05-01 02:01:39 +0300

    add mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,29 +1,42 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+mongoose.connect('mongodb://test:test@ds127341.mlab.com:27341/todos');
 
-var data = [
-  {item: 'walk dog'},
-  {item: 'sent email'},
-];
+var todoSchema = new mongoose.Schema({
+  item: String
+})
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var data = [
+//   {item: 'walk dog'},
+//   {item: 'sent email'},
+// ];
 
 module.exports = function (app) {
 
   app.get('/', function (req, res) {
-    res.render('todo', {data: data});
+    Todo.find({}, function (err, data) {
+      if (err) throw err;
+      res.render('todo', {data: data});
+    })
   })
 
   app.post('/', urlencodedParser, function (req, res) {
     if (req.body.item != '')
-      data.push(req.body);
-    res.send();
+      Todo(req.body).save(function (err, data) {
+        if (err) throw err;
+        res.send();
+      })
   });
 
   app.delete('/:item', function (req, res) {
-    data = data.filter(function (item) {
-      return !(item.item.trim() == req.params.item.trim());
-    });
-    res.send();
+    Todo.find({item: req.params.item.trim()}).remove(function (err, data) {
+      if (err) throw err;
+      res.send();
+    })
   })
 
 
